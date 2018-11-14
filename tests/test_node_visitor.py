93e23f58246869e648b7cacd343f2a1506aeb060
Joshua Doncaster-Marsiglio <josh.doncastermarsiglio@tophatmonocle.com>
2018-11-14 15:56:43 -0500

    add test for node visitor

---
+++ tests/test_node_visitor.py
@@ -0,0 +1,13 @@
+from codewatch import (
+    NodeVisitor,
+    Stats,
+)
+
+
+def test_sets_stats_and_file_path():
+    stats = Stats()
+    file_path = '/mock/path'
+    node_visitor = NodeVisitor(stats, file_path)
+
+    assert node_visitor.stats == stats
+    assert node_visitor.rel_file_path == file_path

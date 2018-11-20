033b46a36ddc3933f0245f5ccb0d3ea4198c3706
Joshua Doncaster-Marsiglio <josh.doncastermarsiglio@tophatmonocle.com>
2018-11-20 15:27:24 -0500

    cant lose coverage :O

--- tests/test_defaults.py
+++ tests/test_defaults.py
@@ -6,24 +6,29 @@ from codewatch.defaults import (
 )
 
 
-@pytest.mark.parametrize('dir_name,expected_output', [
-    ('migrations', False),
-    ('normal_dir', True),
-    ('test_dir', False),
-    ('another_test', False),
-    ('another_normal', True),
+@pytest.mark.parametrize('dir_name,kwargs,expected_output', [
+    ('migrations', {}, False),
+    ('migrations', {'exclude_migration_dirs': False}, True),
+
+    ('normal_dir', {}, True),
+    ('another_normal', {}, True),
+
+    ('test_dir', {}, False),
+    ('test_dir', {'exclude_test_dirs': False}, True),
+    ('another_test', {}, False),
 ])
-def test_default_directory_filter(dir_name, expected_output):
-    dir_filter = create_directory_filter()
+def test_default_directory_filter(dir_name, kwargs, expected_output):
+    dir_filter = create_directory_filter(**kwargs)
     assert dir_filter(dir_name) == expected_output
 
 
-@pytest.mark.parametrize('file_name,expected_output', [
-    ('normal_python_file.py', True),
-    ('non_python_file.txt', False),
-    ('test_py_file.py', False),
-    ('a_pyc_file.pyc', False),
+@pytest.mark.parametrize('file_name,kwargs,expected_output', [
+    ('normal_python_file.py', {}, True),
+    ('non_python_file.txt', {}, False),
+    ('a_pyc_file.pyc', {}, False),
+    ('test_py_file.py', {}, False),
+    ('test_py_file.py', {'exclude_test_files': False}, True),
 ])
-def test_default_file_filter(file_name, expected_output):
-    dir_filter = create_file_filter()
-    assert dir_filter(file_name) == expected_output
+def test_default_file_filter(file_name, kwargs, expected_output):
+    file_filter = create_file_filter(**kwargs)
+    assert file_filter(file_name) == expected_output

10a7f4a1d8cb6074049e8072416d312cdccaec14
Joshua Doncaster-Marsiglio <josh.doncastermarsiglio@tophatmonocle.com>
2018-11-20 16:44:05 -0500

    better API

--- tests/test_defaults.py
+++ tests/test_defaults.py
@@ -6,30 +6,34 @@ from codewatch.defaults import (
 )
 
 
-@pytest.mark.parametrize('dir_name,kwargs,expected_output', [
-    ('migrations', {}, False),
-    ('migrations', {'exclude_migration_dirs': False}, True),
+@pytest.mark.parametrize('dir_name,directory_filters,expected_output', [
+    ('migrations', None, False),
+    ('migrations', [], True),
 
-    ('normal_dir', {}, True),
-    ('another_normal', {}, True),
+    ('normal_dir', None, True),
+    ('another_normal', None, True),
 
-    ('test_dir', {}, False),
-    ('test_dir', {'exclude_test_dirs': False}, True),
-    ('another_test', {}, False),
+    ('test_dir', None, False),
+    ('test_dir', [], True),
+    ('another_test', None, False),
 ])
-def test_create_directory_filter(dir_name, kwargs, expected_output):
-    dir_filter = create_directory_filter(**kwargs)
+def test_create_directory_filter(
+    dir_name,
+    directory_filters,
+    expected_output,
+):
+    dir_filter = create_directory_filter(directory_filters)
     assert dir_filter(dir_name) == expected_output
 
 
-@pytest.mark.parametrize('file_name,kwargs,expected_output', [
-    ('normal_python_file.py', {}, True),
-    ('non_python_file.txt', {}, False),
-    ('non_python_file.txt', {'only_include_py_files': False}, True),
-    ('a_pyc_file.pyc', {}, False),
-    ('test_py_file.py', {}, False),
-    ('test_py_file.py', {'exclude_test_files': False}, True),
+@pytest.mark.parametrize('file_name,file_filters,expected_output', [
+    ('normal_python_file.py', None, True),
+    ('non_python_file.txt', None, False),
+    ('non_python_file.txt', [], True),
+    ('a_pyc_file.pyc', None, False),
+    ('test_py_file.py', None, False),
+    ('test_py_file.py', [], True),
 ])
-def test_create_file_filter(file_name, kwargs, expected_output):
-    file_filter = create_file_filter(**kwargs)
+def test_create_file_filter(file_name, file_filters, expected_output):
+    file_filter = create_file_filter(file_filters)
     assert file_filter(file_name) == expected_output

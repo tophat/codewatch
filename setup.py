20883c30d52be75a6c751af549662f4e2ed7f749
Chris Abiad <chris@tophat.com>
2018-11-22 15:08:52 -0500

    PyPI classifiers and set req'd py versions to 2.7 or 3.6

--- setup.py
+++ setup.py
@@ -3,6 +3,22 @@ from setuptools import (
     setup,
 )
 
+CLASSIFIERS = [
+    "Development Status :: 3 - Alpha",
+    "Intended Audience :: Developers",
+    "Natural Language :: English",
+    "License :: OSI Approved :: Apache Software License",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 2",
+    "Programming Language :: Python :: 2.7",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: Implementation :: CPython",
+    "Topic :: Software Development :: Libraries ",
+]
+
+
 setup(
     name='codewatch',
     packages=find_packages(),
@@ -11,4 +27,5 @@ setup(
     install_requires=[
         'astroid==1.6.4',  # 2.0 onwards is py3 only
     ],
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*, <3.7"
  )

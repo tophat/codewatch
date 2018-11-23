c38a92398b940021f8f0a5db9c4fba5c868f3843
rohit-jain27 <rohit.jain@tophatmonocle.com>
2018-11-23 17:11:48 -0500

    Pypi links and descriptions (#59)
    * add to pylint info
    
    * add README.md file check
    
    * bump version to 0.0.15

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
+import os
 import sys
+from os.path import join, exists
+
 from setuptools import (
     find_packages,
     setup,
@@ -31,11 +34,23 @@ else:
         'astroid==1.6.4',  # 2.0 onwards is py3 only
     ]
 
+base_dir = os.path.dirname(__file__)
+readme_path = join(base_dir, 'README.md')
+if exists(readme_path):
+    with open(readme_path) as stream:
+        long_description = stream.read()
+else:
+    long_description = ''
+
 setup(
     name='codewatch',
     classifiers=CLASSIFIERS,
     packages=find_packages(),
-    version='0.0.14',
+    version='0.0.15',
+    description="Monitor and manage deeply customizable metrics about your python code using ASTs",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tophat/codewatch",
     scripts=['bin/codewatch'],
     install_requires=INSTALL_REQUIRES,
     python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*, <3.8"

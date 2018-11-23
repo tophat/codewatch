99ce0fb26fd5623867acc41105028da18439c2ca
Chris Abiad <cabiad@users.noreply.github.com>
2018-11-23 15:26:31 -0500

    `bin/codewatch exits with -1 on any failures (#57)
    * bump to 0.0.14

--- tests/run/integration/test_bin.py
+++ tests/run/integration/test_bin.py
@@ -8,7 +8,7 @@ from tests.config_modules import (
 
 def test_codewatch_returns_success():
     ret = call(['codewatch', integration_config.__name__])
-    assert ret == 0
+    assert ret == 255
 
 
 def test_codewatch_utf8_returns_success():

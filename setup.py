9eb0f4a753f5f332d82cffad8c544f71984c75f5
Rohit Jain <rohit.jain@tophatmonocle.com>
2018-11-23 12:08:50 -0500

    update astroid requirements

--- setup.py
+++ setup.py
@@ -23,10 +23,10 @@ setup(
     name='codewatch',
     classifiers=CLASSIFIERS,
     packages=find_packages(),
-    version='0.0.12',
+    version='0.0.13',
     scripts=['bin/codewatch'],
     install_requires=[
-        'astroid==1.6.4',  # 2.0 onwards is py3 only
+        'astroid>=1.6,<2.0',  # 2.0 onwards is py3 only
     ],
     python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*, <3.7"
  )

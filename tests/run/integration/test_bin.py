687b27275142f9439b89dc118ff590db3d3ecb97
NoahNU <noahnu@gmail.com>
2018-12-01 11:56:20 -0500

    test: normalize paths and subprocess for win compat

--- tests/run/integration/test_bin.py
+++ tests/run/integration/test_bin.py
@@ -1,3 +1,5 @@
+import os
+import sys
 from subprocess import call
 
 from tests.config_modules import (
@@ -6,11 +8,17 @@ from tests.config_modules import (
 )
 
 
+def _call_codewatch(pargs, **kwargs):
+    pythonexec = sys.executable or 'python'
+    codewatchbin = os.path.normcase('./bin/codewatch')
+    return call([pythonexec, codewatchbin] + pargs, shell=True, **kwargs) & 0xFF
+
+
 def test_codewatch_returns_success():
-    ret = call(['codewatch', integration_config.__name__])
+    ret = _call_codewatch([integration_config.__name__])
     assert ret == 255
 
 
 def test_codewatch_utf8_returns_success():
-    ret = call(['codewatch', integration_config_utf8.__name__])
+    ret = _call_codewatch([integration_config_utf8.__name__])
     assert ret == 0
